8d01e25b70805449f8cfd6361d1b47d22476819f
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-06 13:51:10 +0200

    distance

--- PIntroductionToOOP/PIntroductionToOOP/main.cpp
+++ PIntroductionToOOP/PIntroductionToOOP/main.cpp
@@ -8,13 +8,34 @@ using namespace std;
 
 class Point
 {
+private:
 	double x;
 	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 };
+
+//#define STRUCT_POINT
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
 	cout << "Hello OOP" << endl;
 
 	//type name;
@@ -28,11 +49,37 @@ void main()
 
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 }
 
 /*
 ----------------------------------------------------------
 .  - оператор прямого доступа (Point operator)
 -> - оператор косвенного доступа (Arrow operator)
+----------------------------------------------------------
+*/
+
+/*
+----------------------------------------------------------
+				OOP concepts:
+1. Encapsulation;
+2. Inheritance;
+3. Polymorphism;
+
+				Encapsulation:
+Модификаторы доступа
+private:	закрытые поля, доступные только внутри класса
+public:		открытые поля, доступные из любого места программы
+protected:	защищенные поля, доступны внутри класса, и его дочерних классов
+
+
+get/set-методы
+get - взять, получить
+set - задать, установить
 ----------------------------------------------------------
 */
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,7 @@
 https://github.com/okovtun/VPU_221.git
 https://www.youtube.com/watch?v=1bcvF-OTs5k&list=PLeqyOOqxeiIOLpsRIUc_mkYSn_z7XkG14
 
+TODO:
+1. Выучить теорию!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+2. Реализовать метод ??? distance(???), который возвращает расстояние до указанной точки;
+3. Написать функцию ??? distance(???), которая возвращает расстояние между двумя точками;
