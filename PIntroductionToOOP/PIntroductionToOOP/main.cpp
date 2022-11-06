f66f49d6dd50bf8ea142b0c134f737316c68f9f0
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-06 12:28:52 +0200

    ReadMe

---
+++ PIntroductionToOOP/PIntroductionToOOP/main.cpp
@@ -0,0 +1,38 @@
+﻿//IntroductionToOOP
+#include<iostream>
+using namespace std;
+
+//Объектно-Ориентированное программирование на языке C++
+//ООП
+//OOP - Object-Oriented Programming
+
+class Point
+{
+	double x;
+	double y;
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello OOP" << endl;
+
+	//type name;
+	int a;		//объявление переменной 'a' типа 'int'
+	Point A;	//объявление переменной 'A' типа 'Point'
+				//создание объекта 'A' структуры 'Point'
+				//объявление экземпляра 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+}
+
+/*
+----------------------------------------------------------
+.  - оператор прямого доступа (Point operator)
+-> - оператор косвенного доступа (Arrow operator)
+----------------------------------------------------------
+*/
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+https://github.com/okovtun/VPU_221.git
+https://www.youtube.com/watch?v=1bcvF-OTs5k&list=PLeqyOOqxeiIOLpsRIUc_mkYSn_z7XkG14
+
